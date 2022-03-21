d6c89f205f7d0bdaef0b26ae4a4a7995f0f3eb7d
Mohit Agrawal <mohitagrawalmca@gmail.com>
2022-03-21 23:54:14 +0530

    Add project files.

---
+++ ConfigurationNet5.Sample/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using ConfigurationNet5.Sample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigurationNet5.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private Setting _Setting;
+        public HomeController(ILogger<HomeController> logger, IOptions<Setting> options, IConfiguration  configuration )
+        {
+            _logger = logger;
+            _Setting = options.Value;
+
+            // Type2
+            string config4 =  configuration["Settings:Config4"];
+
+            // Type3
+            string Version = configuration.GetSection("Version").Value;
+            // Type4
+            Version = configuration.GetValue<string>("Version");
+
+            // Type5
+            config4 = configuration.GetSection("Settings").GetSection("Config4").Value;
+
+            Console.Write(config4);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ConfigurationNet5.Sample/Models/AppSettingManager.cs
@@ -0,0 +1,15 @@
+﻿namespace ConfigurationNet5.Sample.Models
+{
+    public class AppSettingManager
+    {
+        public Setting Settings { get; set; }
+
+    }
+
+    public class Setting
+    {
+        public string Config1 { get; set; }
+        public string Config2 { get; set; }
+        public string Config3 { get; set; }
+    }
+}
